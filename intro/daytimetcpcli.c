438e77675bda7c41781c768d6a0e1d2eda7358c7
zhangdi <765350744@qq.com>
2017-12-30 09:04:22 +0800

    commit daytimetcpcli.c without unp.h

---
+++ intro/daytimetcpcli.c
@@ -0,0 +1,53 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXLINE (256)
+int main(int argc, char **argv)
+{
+    // 1. 参数校验
+    if (argc != 2){
+        printf("usage: a.out <IPaddress>\n");
+        exit(0);
+    }
+
+    // 2. 建立socket
+    int sockfd = 0;
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("socket error!\n");
+        exit(0);
+    }
+
+    // 3. 建立连接
+    struct sockaddr_in  addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port   = htons(13);
+    if (inet_pton(AF_INET, argv[1], &addr.sin_addr) < 0){
+        printf("inet_pton error for %s\n", argv[1]);
+        exit(0); 
+    }
+    if ( connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0){
+        printf("connect error!\n");
+        exit(0); 
+    }
+
+    // 4. 读取数据
+    char recvline[MAXLINE+1];
+    int n = 0;
+    while( (n = read(sockfd, recvline, MAXLINE)) > 0 ){
+        recvline [n] = 0;
+        printf("%s\n", recvline);
+    }
+
+    if (n<0){
+        printf("read error!\n");
+    }
+
+    exit(0);
+}
